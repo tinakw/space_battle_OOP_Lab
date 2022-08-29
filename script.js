867f5e833b1678c305fca95a5e2aed92d2b35711
tinakw <110048198+tinakw@users.noreply.github.com>
2022-08-29 13:20:57 -0400

    Space Battle USS Pandora Complete

--- script.js
+++ script.js
@@ -1,91 +1,106 @@
-//Space Battle
+//Space Battle: Java Space Games
 // 
-
-class Ship {
-  constructor(name, hull, firepower, accuracy) {
+class alienShip {
+  constructor(name) {
     this.name = name,
-    this.hull = hull || (Math.floor(Math.random() * 3) + 3,
-    this.firepower = firepower || (Math.random() * 2) + 2,
-    this.accuracy = accuracy || Math.random() * .2) + .6
-
-    // if (Math.random() < alien[0].accuracy) {
-    //   console.log('You have been hit!');
-    // }  
+    this.hull = Math.floor(Math.random() * (7 - 3) + 3),
+    this.firepower = Math.floor(Math.random() * (5 - 2) + 2),
+    this.accuracy = Math.floor(Math.random() * (.8 - .6) + .6)
   }
-}
 
-class heroShip extends Ship {
-  constructor(name, hull, firepower, accuracy) {
-    super(name, hull, firepower, accuracy);
-    // add additional properties for the "Main Ship"
+  attackHero(){
+    let randomNum = Math.floor(Math.random())
+     if (randomNum < this.accuracy) {
+          hero.hull -= this.firepower
+          window.alert(`You were hit for ${this.firepower} damage!`); 
+        } else { 
+          window.alert('Alien missed!') 
+        }
+      }
+};
+
+class heroShip {
+  constructor() {
     this.name = "USS Pandora";
     this.hull = 20,
     this.firepower = 5,
     this.accuracy = .7
   }
-}
-attackAlien(target){
-attackAlien(target){ // attackAlien occurs whenever the player attacks the alien. -- attackMain (on the alien class) occurs whenever the alien attacks the player.
-    let randomNum = Math.random()
-    if (randomNum <= this.accuracy) {
-        console.log("Attack")
+
+  attackAlien(){ // attackAlien occurs whenever the player attacks the alien. -- attackHero (on the alien class) occurs whenever the alien attacks the player.
+    let randomNum = Math.floor(Math.random())
     if (randomNum <= this.accuracy) { // checks if the player hits the alien
-      target.hull -= this.firepower // subtracts the player's firepower from the enemy's health if it hits
-        window.alert(`Attacked ${target.name} for ${this.firepower} damage!`)
+      alien.hull -= this.firepower // subtracts the player's firepower from the enemy's health if it hits
+      window.alert(`You successfully attacked ${alien.name} for ${this.firepower} damage!`)
+    } else {
+      window.alert('You have missed!') // if it doesn't hit, do something here
     }
-    else {
-        (console.log('You have missed!'))
-        (window.alert('You have missed!')) // if it doesn't hit, do something here
+    // after the player attacks, we are going to run the target's (alien's) attackHero method so that it attacks back.
     }
-    // after the player attacks, we are going to run the target's (alien's) attackMain method so that it attacks back.
-    target.attackHero(this)
-}
-}
+};
 
-attackHero(target) {
-  let randomNum = Math.random()
-   if (randomNum < this.accuracy) {
-       console.log('You hit the target!');
-      target.hull -= this.firepower
-      window.alert(`Attacked ${target.name} for ${this.firepower} damage!`); // target is the actual target of the attack. any changes made on target.whatever are going to affect the actual thing given to it.
-       // (to reduce alien's health, think about target.hull)
-   }
-   else { console.log('You have missed!') }
-   else { window.alert('You have missed!') }
+const hero = new heroShip();
+
+const gameBegin = () => {
+  window.alert('Welcome to Java Space Games, Captain!');
+  window.alert('Earth has been attacked by a horde of aliens! Your mission is to destroy every last alien ship.');
+  window.alert('The USS Pandora has the advantage of power and strikes first.');
+  window.alert('Good Luck!');
+};
+
+const stat = () => {
+  window.alert(`Your Hull is ${hero.hull} and the Alien hull is  ${alien.hull}`);
+};
+let alienCount = 6;
+let alienList = [];
+for (let i = alienCount; i > 0; i --){
+  alienList.push("alienShip"+i);
+};
+
+let alienName = ('Alien '+ alienList.length)
+let alien = alienList.pop() 
+alien = new alienShip(alienName)
+
+const prepare = () => {
+  if (alienList.length > 0){
+    if (alien.hull > 0 && hero.hull > 0){
+      window.alert('Ready for Battle!')
+      hero.attackAlien();
+      if (alien.hull > 0){
+        alien.attackHero();
+      }
+      battle();
+    } else if (alien.hull <= 0 && alienList.length > 0){
+      window.alert(`${alien.name} destroyed! Next one is coming!`)
+      alienName = ('Alien '+ alienList.length)
+      alien = alienList.pop()
+      alien = new alienShip(alienName)
+      window.alert(`${alien.name} has advanced and is locked in.`)
+    } else if (alienList.length >= 1 && hero.hull <= 0){
+      window.alert('You have been destroyed!')
+    }
+  } else {
+    window.alert('All aliens eliminated! YOU WIN!')
   }
-}
+};
 
-const USSPandora = new HeroShip 
-  const alienShip1 = new AlienShip ("Alien Ship 1")
-  const alienShip2 = new AlienShip ("Alien Ship 2")
-  const alienShip3 = new AlienShip ("Alien Ship 3")
-  const alienShip4 = new AlienShip ("Alien Ship 4")
-  const alienShip5 = new AlienShip ("Alien Ship 5")
-  const alienShip6 = new AlienShip ("Alien Ship 6")
+const battle = () => {
+  stat();
+  prepare();
+};
 
-
-const allAlien = [alienShip1,alienShip2, alienShip3, alienShip4, alienShip5, alienShip6]
-console.log(allAlien)
-
-USSPandora.attackAlien(alienShip2);
 
 const attackButton = document.getElementById('attack')
-attackButton.addEventListener('click', () => {
-  // check if any enemy is left (if statement -- not a loop)
-      // if so, attack the first one in the array
-      // else, end the game with disabling button and popping up YOU WIN! alert.
-      USSPandora.attackAlien(alienShip4)
+attackButton.addEventListener('click', () => {battle()})
+
+const retreatButton = document.getElementById('retreat')
+retreatButton.addEventListener('click', () => {
+      window.alert('You have doomed humanity by your cowardice!!');
+      hero.hull=0;
+      hero.firepower=0;
+      hero.accuracy=0;
+      stat();
 })
 
-// BEFORE you do the attack button stuff, we need to make it so that when an enemy's hitpoint (hull) is reduced to 0, they die.
-    // The way I would do this is inside of the attackAlien method, at the very end, (before the alien attacks though) you need to check target's hull. If it is 0 or lower (becuase you can deal more damage than it has health sometimes) then get rid of it out of the array. (allAlien).
-    // you can do this with shift().
-
-    // ----------------------
-
-    USSPandora.attackAlien(alienShip4) 
-    // after you get that functioning, you're going to need to work on your attack button.
-    // The attack button should get the first item in the allAlien array, and pass that to your USSAssembly.attackAlien() method.
-    // right before this though, you need to make sure there are aliens left to attack!
-    // if there is no aliens left. You can add a css property to your button with {disabled: true}. this makes it impossible to click the button.
-    // you also might want to pop up an alert saying they are all dead.
+const startButton = document.getElementById('start')
+startButton.addEventListener('click', () => {gameBegin()})
